6ca2b369aa130a32eed358e3bf2af1b4e36e8195
Arthur Queiroz <110779984+arthurqueiroz4@users.noreply.github.com>
2022-12-11 14:37:23 -0300

    verificaMean e relaciona a imagem c a filtrada

--- main.c
+++ main.c
@@ -1,29 +1,27 @@
+#include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <dirent.h>
 #include <string.h>
 #include <time.h>
 
-struct pgm{
-	int tipo;
-	int c;
-	int r;
-	int mv;
-	unsigned char *pData;
+struct pgm {
+  int tipo;
+  int c;
+  int r;
+  int mv;
+  unsigned char *pData;
 };
 
 void readPGMImage(struct pgm *, char *);
 void viewPGMImage(struct pgm *);
 void writePGMImage(struct pgm *, char *);
-void esta_contido(char [],char []);
+char verificarMean(char []);
 
-int main(){
-  
+int main() {
+
   DIR *d, *d_mean;
-  char *comp;
-	struct pgm img;
-  struct dirent *dir;
-  struct dirent *dir_mean;
+  struct pgm img, img_mean;
+  struct dirent *dir, *dir_mean;
   d = opendir("./images");
   d_mean = opendir("./images");
   rewinddir(d);
@@ -31,47 +29,44 @@ int main(){
   int cont = 0;
   int k;
   clock_t begin, end;
-  double time_per_img, time_total=0;
+  double time_per_img, time_total = 0;
   // mean -> strcmp("mean", d_name);
-  
-  if (d){
-    //while ((dir = readdir(d)) != NULL){
-    for(int i=0;(dir = readdir(d)) != NULL; i+=2){
-    
-      begin = clock();  //0 -> 3 -> 1 -> 5 -> 9 -> 7 -> 6 -> 10 -> 17 -> 13
-      cont +=1;
-      
-      if (i >= 3){
-    
+  char id[50], nome_arquivo[50], mean[10]="mean",itsmean, nome_arquivo_mean[50];
+  if (d) {
+    // while ((dir = readdir(d)) != NULL){
+    for (int i = 0; (dir = readdir(d)) != NULL; i += 2) {
+
+      begin = clock(); // 0 -> 3 -> 1 -> 5 -> 9 -> 7 -> 6 -> 10 -> 17 -> 13
+      cont += 1;
+
+      if (i >= 3) {
+
         
-        char id[50], nome_arquivo[50], mean[50]="mean", itsmean;
         strncpy(id, dir->d_name + 2, 5);
         strcpy(nome_arquivo, dir->d_name);
-        printf("nome do arquivo: %s \n", nome_arquivo);
-        printf("id do arquivo: %s \n", id);
-        if (strstr(nome_arquivo, mean)!=NULL){
-          itsmean = 's';
-          printf("tipo mean: %c\n", itsmean);
-        } else {
-          itsmean = 'n';
-          printf("tipo mean: %c\n", itsmean);
-        }
-        puts("");
-        
-        /*for(int z=0;(dir_mean = readdir(d_mean))!=NULL;z++){
-          if (z>=2){
-            printf("%s\n", dir_mean -> d_name);
-            comp = strstr(nome_arquivo, "mean");
-            if (comp == NULL){
-              strcpy(mean, "ok");
-            }
+        itsmean = verificarMean(nome_arquivo);
+        if (itsmean == 'n'){
+          for(int x=0;(dir_mean=readdir(d_mean))!=NULL;x++){
+            if (x>=2){
+              if ((strstr(dir_mean->d_name, id)!=NULL) && (strstr(dir_mean->d_name, mean)!=NULL)){
+                strcpy(nome_arquivo_mean, dir_mean->d_name);
+              }
+            }  
           }
-       }*/
+          rewinddir(d_mean);
+        } else {
+          continue;
+        }
+        printf("nome do arquivo: %s \n", nome_arquivo);
+        printf("arquivo filtrado: %s\n", nome_arquivo_mean);
+        printf("id do arquivo: %s \n", id);
+        printf("tipo mean: %c", itsmean);
+        char saida[50] = "saida";
+        readPGMImage(&img, nome_arquivo);
         
-        //printf("id: %s || mean: %s\n", id, mean);
-        readPGMImage(&img, dir->d_name);
-        //writePGMImage(&img, "saida.pgm");
-        //viewPGMImage(&img);
+        writePGMImage(&img, strcat(saida, nome_arquivo));
+        viewPGMImage(&img);
+        puts("-------------------------------------------");
         /*if(strlen(dir->d_name) > 23){
           puts("mean");
         }*/
@@ -84,91 +79,98 @@ int main(){
     }
     closedir(d);
   }
-  cont -= 2; //cont fica com o total de arquivos armazenado
-  printf("Tempo médio: %lf\n",time_total/cont); //tempo total/total de arquivos(cont)
-	printf("Tempo Total: %lf\n",time_total);
-	return 0;
-
+  cont -= 2; // cont fica com o total de arquivos armazenado
+  printf("Tempo médio: %lf\n",
+         time_total / cont); // tempo total/total de arquivos(cont)
+  printf("Tempo Total: %lf\n", time_total);
+  return 0;
 }
 
-void readPGMImage(struct pgm *pio, char *filename){
+void readPGMImage(struct pgm *pio, char *filename) {
 
-	FILE *fp;
-	char ch;
+  FILE *fp;
+  char ch;
   char diretorio[50] = "images/";
 
-	if (!(fp = fopen(strcat(diretorio, filename),"r"))){
-		perror("Erro.");
-		exit(1);
-	}
+  if (!(fp = fopen(strcat(diretorio, filename), "r"))) {
+    perror("Erro.");
+    exit(1);
+  }
 
-	if ( (ch = getc(fp))!='P'){
-		puts("A imagem fornecida não está no formato pgm");
-		exit(2);
-	}
-	
-	pio->tipo = getc(fp)-48;
-	
-	fseek(fp,1, SEEK_CUR);
+  if ((ch = getc(fp)) != 'P') {
+    puts("A imagem fornecida não está no formato pgm");
+    exit(2);
+  }
 
-	while((ch=getc(fp))=='#'){
-		while( (ch=getc(fp))!='\n');
-	}
+  pio->tipo = getc(fp) - 48;
 
-	fseek(fp,-1, SEEK_CUR);
+  fseek(fp, 1, SEEK_CUR);
 
-	fscanf(fp, "%d %d",&pio->c,&pio->r);
+  while ((ch = getc(fp)) == '#') {
+    while ((ch = getc(fp)) != '\n')
+      ;
+  }
 
-	if (ferror(fp)){ 
-		perror(NULL);
-		exit(3);
-	}	
-	fscanf(fp, "%d",&pio->mv);
-	fseek(fp,1, SEEK_CUR);
+  fseek(fp, -1, SEEK_CUR);
 
-	pio->pData = (unsigned char*) malloc(pio->r * pio->c * sizeof(unsigned char));
+  fscanf(fp, "%d %d", &pio->c, &pio->r);
 
-	switch(pio->tipo){
-		case 2:
-			puts("Lendo imagem PGM (dados em texto)");
-			for (int k=0; k < (pio->r * pio->c); k++){
-				fscanf(fp, "%hhu", pio->pData+k);
-			}
-		break;	
-		case 5:
-			puts("Lendo imagem PGM (dados em binário)");
-			fread(pio->pData,sizeof(unsigned char),pio->r * pio->c, fp);
-		break;
-		default:
-			puts("Não está implementado");
-	}
-	
-	fclose(fp);
+  if (ferror(fp)) {
+    perror(NULL);
+    exit(3);
+  }
+  fscanf(fp, "%d", &pio->mv);
+  fseek(fp, 1, SEEK_CUR);
 
+  pio->pData = (unsigned char *)malloc(pio->r * pio->c * sizeof(unsigned char));
+
+  switch (pio->tipo) {
+  case 2:
+    puts("Lendo imagem PGM (dados em texto)");
+    for (int k = 0; k < (pio->r * pio->c); k++) {
+      fscanf(fp, "%hhu", pio->pData + k);
+    }
+    break;
+  case 5:
+    puts("Lendo imagem PGM (dados em binário)");
+    fread(pio->pData, sizeof(unsigned char), pio->r * pio->c, fp);
+    break;
+  default:
+    puts("Não está implementado");
+  }
+
+  fclose(fp);
 }
 
-void writePGMImage(struct pgm *pio, char *filename){
-	FILE *fp;
-	char ch;
+void writePGMImage(struct pgm *pio, char *filename) {
+  FILE *fp;
+  char ch;
 
-	if (!(fp = fopen(filename,"wb"))){
-		perror("Erro.");
-		exit(1);
-	}
+  if (!(fp = fopen(filename, "wb"))) {
+    perror("Erro.");
+    exit(1);
+  }
 
-	fprintf(fp, "%s\n","P5");
-	fprintf(fp, "%d %d\n",pio->c, pio->r);
-	fprintf(fp, "%d\n", 255);
+  fprintf(fp, "%s\n", "P5");
+  fprintf(fp, "%d %d\n", pio->c, pio->r);
+  fprintf(fp, "%d\n", 255);
 
-	fwrite(pio->pData, sizeof(unsigned char),pio->c * pio->r, fp);
+  fwrite(pio->pData, sizeof(unsigned char), pio->c * pio->r, fp);
 
-	fclose(fp);
-
+  fclose(fp);
 }
 
+void viewPGMImage(struct pgm *pio) {
+  printf("Tipo: %d\n", pio->tipo);
+  printf("Dimensões: [%d %d]\n", pio->c, pio->r);
+  printf("Max: %d\n", pio->mv);
+}
 
-void viewPGMImage(struct pgm *pio){
-	printf("Tipo: %d\n",pio->tipo);
-	printf("Dimensões: [%d %d]\n",pio->c, pio->r);
-	printf("Max: %d\n",pio->mv);
+char verificarMean(char nome_arquivo[50]){
+  char mean[50] = "mean";
+  if (strstr(nome_arquivo, mean) != NULL) {
+      return 's';
+  } else {
+      return 'n';
+  }
 }
